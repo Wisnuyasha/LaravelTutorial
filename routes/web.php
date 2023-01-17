0d22cdc409d5125a8888313391c2be28b2696b10
treeoon <wisnuyeahfaizal@gmail.com>
2023-01-17 15:21:49 +0700

    frontend contact

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Home\PortfolioController;
 use App\Http\Controllers\Home\BlogCategoryController;
 use App\Http\Controllers\Home\BlogController;
 use App\Http\Controllers\Home\FooterController;
+use App\Http\Controllers\Home\ContactController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -93,6 +94,11 @@ Route::controller(BlogController::class)->group(function() {
 Route::controller(FooterController::class)->group(function() {
     Route::get('/footer/setup', 'FooterSetup')->name('footer.setup');
     Route::post('/update/footer=', 'UpdateFooter')->name('update.footer');
+});
+
+Route::controller(ContactController::class)->group(function() {
+    Route::get('/contact', 'Contact')->name('contact.me');
+    Route::post('/store/message', 'StoreMessage')->name('store.message'); 
 });
 
 
