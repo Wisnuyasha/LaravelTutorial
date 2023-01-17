8f31b4f743fd84ff2a724a9a3b365f39e51b0b55
treeoon <wisnuyeahfaizal@gmail.com>
2023-01-17 19:02:19 +0700

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,15 @@ Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware('auth')->name('dashboard');
 
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin/logout', 'destroy')->name('admin.logout');
-    Route::get('/admin/profile', 'Profile')->name('admin.profile');
-    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
-    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
-    Route::get('/change/password', 'ChangePassword')->name('change.password');
-    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+Route::middleware(['auth'])->group(function (){
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/logout', 'destroy')->name('admin.logout');
+        Route::get('/admin/profile', 'Profile')->name('admin.profile');
+        Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+        Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+        Route::get('/change/password', 'ChangePassword')->name('change.password');
+        Route::post('/update/password', 'UpdatePassword')->name('update.password');
+    });
 });
 
 Route::controller(HomeSliderController::class)->group(function() {
